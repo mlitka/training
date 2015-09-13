f16715f9734aa06323fe5642870b51c5a050d8e7
Martyna Litkowska <martyna.litkow@gmail.com>
2015-09-13 19:59:47 +0200

    nareszcie to zadanko!

---
+++ myProject2/src/main/java/edu/pwr/mlitka/myApp/AppController.java
@@ -0,0 +1,14 @@
+package edu.pwr.mlitka.myApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+
+}
---
+++ myProject2/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+<h2 align = "center">Zażółć gęślą jaźń <br /> Martynka Litkowska</h2>
+</body>
+</html>
