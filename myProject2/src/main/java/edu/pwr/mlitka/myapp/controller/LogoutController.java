981168cc2b38aa4baa02fb7d8d24ee4b5c51a43f
Martyna Litkowska <martyna.litkow@gmail.com>
2015-09-28 22:43:29 +0200

    hidden pass in login

--- myProject2/src/main/java/edu/pwr/mlitka/myapp/controller/LogoutController.java
+++ myProject2/src/main/java/edu/pwr/mlitka/myapp/controller/LogoutController.java
@@ -7,20 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.pwr.mlitka.myapp.service.SessionService;
-import edu.pwr.mlitka.myapp.service.UserService;
 
 @Controller
 public class LogoutController {
 
-	@Autowired
-	UserService userService;
 	@Autowired
 	SessionService sessionService;
 
 	@RequestMapping(value = "/logout")
 	String logout(final HttpSession httpSession) {
 
-		userService.deleteUser(sessionService.getLoggedUser(httpSession));
 		this.sessionService.logOutUser(httpSession);
 		return "logout";
 	}
--- myProject2/src/main/webapp/WEB-INF/jsp/login.jsp
+++ myProject2/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
-	<meta charset="UTF-8">
 </head>
 <body>
-<h2 align = "center">login</h2>
+	<h2 align="center">Login</h2>
+	<div align="center">
+		<br />
+
+		<form action="" method="post">
+			Username:<br> <input type="text" name="username"> <br>
+			<br> Password:<br> <input type="password" name="password">
+			<br> <br> <input type="submit" value="Login">
+		</form>
+	</div>
 
 </body>
 </html>
